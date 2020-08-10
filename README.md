1868d2ca8b44107eba34bdee8d0edb678f0f2aeb
Akihiko Koizuka <koizuka@gmail.com>
2020-08-10 13:21:01 +0900

    initial

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # kiriban
-タグレポのキリ番通知を集めるスクリプト
+
+ニコニコ動画のタグレポからキリ番通知を収集して jsonファイルにするスクリプトです。
+
+手抜きです。
+
+## 準備
+- 以下をインストール
+    - [git for windows](https://gitforwindows.org/)
+    - [jq](https://stedolan.github.io/jq/)
+    - [node.js](https://nodejs.org/ja/)
+    - [yarn](https://classic.yarnpkg.com/ja/)
+        - URLEncodeするためだけにnode.js + yarn使ってるので、置き換えられたら依存なくせます
+
+- git bashでリポジトリを展開して、以下を実行
+    - `yarn install`
+
+## 実行方法
+- タグを引数にして kiriban.sh を実行する。
+- `./kiriban.sh factorio`
+- `factorio.json` が生成される。
+
+## 結果
+こんなかんじ
+
+```json
+{
+  "tag": "factorio",
+  "kiriban": [
+    {
+      "date": "2020-07-22T06:06:20.630+09:00",
+      "video": {
+        "id": "sm33532839",
+        "title": "Factorioゆっくり解説プレイ 02 - スループットと精錬場"
+      },
+      "viewCount": 10000
+    },
+    {
+      "date": "2020-07-19T10:11:54.249+09:00",
+      "video": {
+        "id": "sm28466408",
+        "title": "【Factorio】姉妹で遊ぶ工場建築ゲー 後編【VOICEROID実況】"
+      },
+      "viewCount": 100000
+    }
+  ],
+  "foundSince": "2020-07-12T17:51:03+09:00"
+}
+```
+
+## カスタマイズ
+- kiriban.sh の `max_pages` を増やすと最大探索ページ数が増えます。増やしすぎるとサーバー攻撃になるので注意
+- kirban.sh の `days` を増やすと過去 days 日分取得できたら停止します。
+
+## License
+- GPLv3
+
+## Author
+@koizuka
---
+++ kiriban.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# 最大ページ数
+max_pages=20
+
+# 何日前まで遡るか
+days=7
+
+# 連絡先の分かる情報
+user_agent="https://github.com/koizuka/kiriban"
+
+if [[ "$1" == "" ]]; then
+  echo "タグを指定してください" >&2
+  exit 1
+fi
+TAGS=$@
+
+
+NOW=$(date +%s)
+LIMIT_TIME=$(( $NOW - $days * (24*60*60) ))
+
+function urlencode() {
+    yarn -s urlencode $1
+}
+
+function get_tags_timeline() {
+  TAG=$1
+  NEXT=$2
+  URL="https://www.nicovideo.jp/api/tagrepo/v2/timeline?tags=$(urlencode $TAG)"
+  if [[ "$NEXT" != "" ]]; then
+    URL="$URL&untilId=$NEXT"
+  fi
+  curl -A "$userAgent" -s "$URL"
+}
+
+function time_to_iso8601() {
+  date -d @$1 --iso-8601=seconds
+}
+
+echo "$days 日前まで探索します。最大 $max_pages ページ" >&2
+
+[[ -d ./log ]] || mkdir log
+
+for TAG in $TAGS; do
+  echo "tag: $TAG" >&2
+  NEXT=
+  temps=
+  for i in $(seq 1 $max_pages); do
+    echo $i/$max_pages >&2
+    FILENAME=log/$TAG.$i.json
+    get_tags_timeline $TAG $NEXT| jq . > $FILENAME
+    NUMOBJS=$(jq -r '.data | length | tonumber' $FILENAME)
+    if [[ $NUMOBJS -eq 0 ]]; then
+      rm $FILENAME
+      break
+    fi
+
+    OLDEST_TIME=$(date -d "$(jq -r '.data[-1].createdAt' $FILENAME)" +%s)
+    echo "$(time_to_iso8601 $OLDEST_TIME) まで" >&2
+
+    tempfile=log/temp.$i.json
+    jq '[.data[] |select(.topic == \"nicovideo.user.video.kiriban.play\" ) | {date:.createdAt, video: {id:.video.id, title:.video.title}, viewCount:.actionLog.viewCount}]' $FILENAME > $tempfile
+
+    NEXT="&untilId=$(jq -r .meta.minId $FILENAME)"
+    temps="$temps $tempfile"
+
+    if [[ $OLDEST_TIME -lt $LIMIT_TIME ]]; then
+      break
+    fi
+    sleep 1 # サーバーにやさしく
+  done
+  jq -n '{tag: \"'$TAG'\", kiriban:[inputs[]], foundSince: \"'$(time_to_iso8601 $OLDEST_TIME)'\"}' $temps > $TAG.json
+  rm $temps
+  echo generated: $TAG.json
+done
